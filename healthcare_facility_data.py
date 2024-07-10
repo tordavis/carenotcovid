144d98034040d4ebcec0caa75971f451ba027ba4
Tori Davis <tori.davis@berkeley.edu>
2024-07-10 16:52:10 -0500

    trying to fix this lol

--- healthcare_facility_data.py
+++ healthcare_facility_data.py
@@ -10,7 +10,7 @@ import pandas as pd
 import csv
 import numpy as np
 import random
-import st-gsheets-connection
+import gsheets-connection
 
 ##############################################################################
 
